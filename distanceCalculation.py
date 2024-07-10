4955a91a0544576dcf0ac083472ed99456264552
tjurjevich <48227792+tjurjevich@users.noreply.github.com>
2024-07-10 17:48:53 -0500

    Update distanceCalculation.py

--- distanceCalculation.py
+++ distanceCalculation.py
@@ -17,7 +17,7 @@ schools = ['Butler', 'UConn', 'Creighton', 'DePaul', 'Georgetown', 'Marquette', 
 addresses = ['510 W 49th St, Indianapolis, IN 46208','2098 Hillside Rd, Storrs, CT 06268','455 N 10th St, Omaha, NE 68102','200 East Cermak Road, Chicago, IL 60616','1340 West Rd, Washington, DC 20057','770 N 12th St, Milwaukee, WI 53233','1 La Salle Square, Providence, RI 02903','4 Pennsylvania Plaza, New York, NY 10001','25 Lafayette Street, Newark, NJ 07102','800 E Lancaster Ave, Villanova, PA 19085','1624 Herald Ave, Cincinnati, OH 45207']
 
 #google distance matrix api key
-google_key = 'AIzaSyCZ5C3CHIT7eEWvf0NUqJ9fVD0gq6sfiQI'
+google_key = '[google api key goes here]'
 
 #create string to use for destinations in api call
 destinations = '%7C'.join([re.sub('( |, )', '+', i) for i in addresses])
